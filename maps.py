734cdf63881108174fb00cc4b71f0b4a73556a9d
hjabbot <42661345+hjabbot@users.noreply.github.com>
2025-03-11 12:03:15 +0000

    Adding terminator map object

--- maps.py
+++ maps.py
@@ -1,3 +1,9 @@
+from locations import City, Country, Terminator
+from trips import Trip
+import plotly.express as px
+
+import geopandas as gpd
+
 class CombinedMap:
     pass
 
@@ -11,4 +17,18 @@ class TripMap:
     pass
 
 class TerminatorMap:
-    pass
+    def __init__(self, time):
+        self.time = time
+        self.terminator = Terminator(self.time)
+        self.df = gpd.GeoDataFrame(
+                    geometry    = [self.terminator.polygon],
+                    index       = [0],
+                    data        = {'tod': 'night'}
+                )
+        self.fig = px.choropleth(
+                    self.df,
+                    geojson     = self.df.geometry,
+                    locations   = self.df.index,
+                    color       = 'tod',
+                    color_discrete_map = 'rgba(0,0,0,0.5)'
+        )
